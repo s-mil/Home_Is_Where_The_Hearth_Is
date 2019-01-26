2af0a8d6e0969eb269b2fc009f66583ed63b7a3d
Timothy Ratliff <timothykratliff@gmail.com>
2019-01-26 17:33:54 +0000

    updated tile palette and started sign interact script

---
+++ Assets/Scripts/InteractOnCollision2D.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Events;
+
+[RequireComponent(typeof(Collider2D))]
+public class InteractOnCollision2D : MonoBehaviour
+{
+    public LayerMask layers;
+    public UnityEvent OnCollision;
+
+    void Reset()
+    {
+        layers = LayerMask.NameToLayer("Everything");
+    }
+
+    // void OnCollisionEnter2D (Collision2D collision)
+    // {
+    //     if (layers.Contains (collision.gameObject))
+    //     {
+    //         OnCollision.Invoke();
+    //     }
+    // }
+
+    void OnDrawGizmos()
+    {
+        Gizmos.DrawIcon(transform.position, "InteractionTrigger", false);
+    }
+
+}
