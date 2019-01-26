b5aa216aec75482cdedd222e79524273b5439935
Timothy Ratliff <timothykratliff@gmail.com>
2019-01-26 19:13:01 +0000

    added grid/tilemap prefab, still working on dialogue

--- Assets/Scripts/InteractOnCollision2D.cs
+++ Assets/Scripts/InteractOnCollision2D.cs
@@ -1,30 +1,28 @@
-﻿using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
-using UnityEngine.Events;
+﻿// using UnityEngine;
+// using UnityEngine.Events;
 
-[RequireComponent(typeof(Collider2D))]
-public class InteractOnCollision2D : MonoBehaviour
-{
-    public LayerMask layers;
-    public UnityEvent OnCollision;
 
-    void Reset()
-    {
-        layers = LayerMask.NameToLayer("Everything");
-    }
+// [RequireComponent(typeof(Collider2D))]
+// public class InteractOnCollision2D : MonoBehaviour
+// {
+//     public LayerMask layers;
+//     public UnityEvent OnCollision;
 
-    void OnCollisionEnter2D (Collision2D collision)
-    {
-        if (layers.Contains (collision.gameObject))
-        {
-            OnCollision.Invoke();
-        }
-    }
+//     void Reset()
+//     {
+//         layers = LayerMask.NameToLayer("Everything");
+//     }
 
-    void OnDrawGizmos()
-    {
-        Gizmos.DrawIcon(transform.position, "InteractionTrigger", false);
-    }
+//     void OnCollisionEnter2D (Collision2D collision)
+//     {
+//         if (layers.Contains (collision.gameObject))
+//         {
+//             OnCollision.Invoke();
+//         }
+//     }
 
-}
+//     void OnDrawGizmos()
+//     {
+//         Gizmos.DrawIcon(transform.position, "InteractionTrigger", false);
+//     }
+// }
--- Assets/Scripts/InteractOnTrigger2D.cs
+++ Assets/Scripts/InteractOnTrigger2D.cs
@@ -1,60 +1,61 @@
-﻿using UnityEngine;
-using UnityEngine.Events;
+﻿// using UnityEngine;
+// using UnityEngine.Events;
 
-[RequireComponent(typeof(Collider2D))]
-public class InteractOnTrigger2D : MonoBehaviour
-{
-    public LayerMask layers;
-    public UnityEvent OnEnter, OnExit;
-    // public InventoryController.InventoryChecker[] inventoryChecks;
 
-    protected Collider2D m_Collider;
+// [RequireComponent(typeof(Collider2D))]
+// public class InteractOnTrigger2D : MonoBehaviour
+// {
+//     public LayerMask layers;
+//     public UnityEvent OnEnter, OnExit;
+//     // public InventoryController.InventoryChecker[] inventoryChecks;
 
-    void Reset()
-    {
-        layers = LayerMask.NameToLayer("Everything");
-        m_Collider = GetComponent<Collider2D>();
-        m_Collider.isTrigger = true;
-    }
+//     protected Collider2D m_Collider;
 
-    void OnTriggerEnter2D(Collider2D other)
-    {
-        if(!enabled)
-            return;
+//     void Reset()
+//     {
+//         layers = LayerMask.NameToLayer("Everything");
+//         m_Collider = GetComponent<Collider2D>();
+//         m_Collider.isTrigger = true;
+//     }
+
+//     void OnTriggerEnter2D(Collider2D other)
+//     {
+//         if(!enabled)
+//             return;
     
-        // if (layers.Contains(other.gameObject))
-        // {
-        //     ExecuteOnEnter(other);
-        // }
-    }
+//         if (layers.Contains(other.gameObject))
+//         {
+//             ExecuteOnEnter(other);
+//         }
+//     }
 
-    void OnTriggerExit2D(Collider2D other)
-    {
-        if(!enabled)
-            return;
+//     void OnTriggerExit2D(Collider2D other)
+//     {
+//         if(!enabled)
+//             return;
     
-        // if (layers.Contains(other.gameObject))
-        // {
-        //     ExecuteOnExit(other);
-        // }
-    }
+//         if (layers.Contains(other.gameObject))
+//         {
+//             ExecuteOnExit(other);
+//         }
+//     }
 
-    protected virtual void ExecuteOnEnter(Collider2D other)
-    {
-        OnEnter.Invoke();
-        // for (int i = 0; i < inventoryChecks.Length; i++)
-        // {
-        //     inventoryChecks[i].CheckInventory(other.GetComponentInChildren<InventoryController>());
-        // }
-    }
+//     protected virtual void ExecuteOnEnter(Collider2D other)
+//     {
+//         OnEnter.Invoke();
+//         // for (int i = 0; i < inventoryChecks.Length; i++)
+//         // {
+//         //     inventoryChecks[i].CheckInventory(other.GetComponentInChildren<InventoryController>());
+//         // }
+//     }
 
-    protected virtual void ExecuteOnExit(Collider2D other)
-    {
-        OnExit.Invoke();
-    }
+//     protected virtual void ExecuteOnExit(Collider2D other)
+//     {
+//         OnExit.Invoke();
+//     }
 
-    void OnDrawGizmos()
-    {
-        Gizmos.DrawIcon(transform.position, "InteractionTrigger", false);
-    }
-}
+//     void OnDrawGizmos()
+//     {
+//         Gizmos.DrawIcon(transform.position, "InteractionTrigger", false);
+//     }
+// }
