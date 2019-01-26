8b38dcbdc93146d3c299e09ce72714406dc886c9
Jackson Oberkrich <jacksonpoberkirch@gmail.com>
2019-01-26 01:18:05 +0000

    No Comments

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -7,7 +7,9 @@ public class PlayerController : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        
+        float moveHorizontal = Input.GetAxis("Horizontal");
+
+
     }
 
     // Update is called once per frame
