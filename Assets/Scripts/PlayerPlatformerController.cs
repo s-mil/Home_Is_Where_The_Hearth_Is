dbbde989335c9f24440ee168803617d885da1e70
Jackson Oberkrich <jacksonpoberkirch@gmail.com>
2019-01-26 06:04:51 +0000

    player movement

--- Assets/Scripts/PlayerPlatformerController.cs
+++ Assets/Scripts/PlayerPlatformerController.cs
@@ -36,11 +36,11 @@ public class PlayerPlatformerController : PhysicsObject
             }
         }
 
-        bool flipSprite = (spriteRenderer.flipX ? (move.x > 0.01f) : (move.x < 0.01f));
-        if (flipSprite)
-        {
-            spriteRenderer.flipX = !spriteRenderer.flipX;
-        }
+            //bool flipSprite = (spriteRenderer.flipX ? (move.x > 0.01f) : (move.x < 0.01f));
+            //if (flipSprite)
+            //{
+            //    spriteRenderer.flipX = !spriteRenderer.flipX;
+            //}
 
         animator.SetBool("grounded", grounded);
         animator.SetFloat("velocityX", Mathf.Abs(velocity.x) / maxSpeed);
