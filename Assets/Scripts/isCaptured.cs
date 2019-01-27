83d1b7fcce540fa9e762e2f65381fe9b40abc98d
Timothy Ratliff <timothykratliff@gmail.com>
2019-01-27 05:54:16 +0000

    add key item to roof06

---
+++ Assets/Scripts/isCaptured.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class isCaptured : MonoBehaviour
+{
+    Animator anim;
+
+    void Start()
+    {
+        anim = GetComponent<Animator>();
+    }
+
+    void OnTriggerEnter2D (Collider2D collision)
+    {
+        if(collision.gameObject.tag == "Player");
+        {
+            Debug.Log("capture collision detected!");
+            anim.SetTrigger("isCaptured");
+        }
+    }
+}
