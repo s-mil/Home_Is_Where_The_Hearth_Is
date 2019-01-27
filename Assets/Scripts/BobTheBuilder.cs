329dca8adb81f86faf302f518cb4d162865ba7eb
Sam Miller <sithpow@gmail.com>
2019-01-27 18:56:28 +0000

    BobTheBuilder Is in Town

---
+++ Assets/Scripts/BobTheBuilder.cs
@@ -0,0 +1,110 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+/// <summary>
+/// On load of the Home Scene determines which pieces of the house should be enabled/disabled
+/// </summary>
+public class BobTheBuilder : MonoBehaviour
+{
+    public GameObject FloorL, FloorR, WallL, WallR, Roof, door, window1, Fillers, Bonfire = null;
+
+    public GameObject WP1, WP2, WP3, WP4, WP5, WP6, WP7, Chim = null;
+    // Start is called before the first frame update
+
+    void Start()
+    {
+
+        int pieces = FindObjectOfType<DungeonMaster>().GetProgress();
+        Debug.Log("Disabling House Objects");
+        FloorL.GetComponent<Renderer>().enabled = false;
+        FloorR.GetComponent<Renderer>().enabled = false;
+        WallL.GetComponent<Renderer>().enabled = false;
+        WallR.GetComponent<Renderer>().enabled = false;
+        fillerDisable();
+        Roof.GetComponent<Renderer>().enabled = false;
+        door.GetComponent<Renderer>().enabled = false;
+        window1.GetComponent<Renderer>().enabled = false;
+        Bonfire.GetComponent<Renderer>().enabled = false;
+        Debug.Log("Case : " + pieces);
+        switch (pieces)
+        {
+            case 0:
+
+                //Nothing Enabled
+                break;
+            case 1:
+                FloorL.GetComponent<Renderer>().enabled = true;
+                FloorR.GetComponent<Renderer>().enabled = true;
+                break;
+            case 2:
+                FloorL.GetComponent<Renderer>().enabled = true;
+                FloorR.GetComponent<Renderer>().enabled = true;
+                WallL.GetComponent<Renderer>().enabled = true;
+                WallR.GetComponent<Renderer>().enabled = true;
+                break;
+            case 3:
+                FloorL.GetComponent<Renderer>().enabled = true;
+                FloorR.GetComponent<Renderer>().enabled = true;
+                WallL.GetComponent<Renderer>().enabled = true;
+                WallR.GetComponent<Renderer>().enabled = true;
+                fillerEnable();
+                door.GetComponent<Renderer>().enabled = true;
+                break;
+            case 4:
+                FloorL.GetComponent<Renderer>().enabled = true;
+                FloorR.GetComponent<Renderer>().enabled = true;
+                WallL.GetComponent<Renderer>().enabled = true;
+                WallR.GetComponent<Renderer>().enabled = true;
+                Fillers.GetComponent<Renderer>().enabled = true;
+                door.GetComponent<Renderer>().enabled = true;
+                break;
+            case 5:
+                FloorL.GetComponent<Renderer>().enabled = true;
+                FloorR.GetComponent<Renderer>().enabled = true;
+                WallL.GetComponent<Renderer>().enabled = true;
+                WallR.GetComponent<Renderer>().enabled = true;
+                fillerEnable();
+                door.GetComponent<Renderer>().enabled = true;
+                window1.GetComponent<Renderer>().enabled = true;
+                break;
+            default:
+                FloorL.GetComponent<Renderer>().enabled = true;
+                FloorR.GetComponent<Renderer>().enabled = true;
+                WallL.GetComponent<Renderer>().enabled = true;
+                WallR.GetComponent<Renderer>().enabled = true;
+                fillerEnable();
+                door.GetComponent<Renderer>().enabled = true;
+                window1.GetComponent<Renderer>().enabled = true;
+                Bonfire.GetComponent<Renderer>().enabled = true;
+                break;
+        }
+
+    }
+
+    void fillerDisable()
+    {
+        WP1.GetComponent<Renderer>().enabled = false;
+        WP2.GetComponent<Renderer>().enabled = false;
+        WP3.GetComponent<Renderer>().enabled = false;
+        WP4.GetComponent<Renderer>().enabled = false;
+        WP5.GetComponent<Renderer>().enabled = false;
+        WP6.GetComponent<Renderer>().enabled = false;
+        WP7.GetComponent<Renderer>().enabled = false;
+        Chim.GetComponent<Renderer>().enabled = false;
+
+    }
+
+    void fillerEnable()
+    {
+        WP1.GetComponent<Renderer>().enabled = true;
+        WP2.GetComponent<Renderer>().enabled = true;
+        WP3.GetComponent<Renderer>().enabled = true;
+        WP4.GetComponent<Renderer>().enabled = true;
+        WP5.GetComponent<Renderer>().enabled = true;
+        WP6.GetComponent<Renderer>().enabled = true;
+        WP7.GetComponent<Renderer>().enabled = true;
+        Chim.GetComponent<Renderer>().enabled = true;
+    }
+
+}
--- Assets/Scripts/DungeonMaster.cs
+++ Assets/Scripts/DungeonMaster.cs
@@ -5,10 +5,19 @@ using UnityEngine.SceneManagement;
 
 public class DungeonMaster : MonoBehaviour
 {
-
+    /// <summary>
+    /// The number of key Items retrieved
+    /// </summary>
     public static int progress = 0;
+    /// <summary>
+    /// The Index the next scene to load
+    /// </summary>
     public static int level = 2;
 
+    /// <summary>
+    /// THe order of the scenes to load
+    /// </summary>
+    /// <value>The index of the scene to load</value>
     Dictionary<int, int> LevelList = new Dictionary<int, int>
     {
         { 1 , 1 },
@@ -37,7 +46,9 @@ public class DungeonMaster : MonoBehaviour
         { 24, 2 },
         { 25, 20}
     };
-
+    /// <summary>
+    /// Dont destroy this object
+    /// </summary>
     private void Awake()
     {
 
@@ -50,23 +61,32 @@ public class DungeonMaster : MonoBehaviour
     /// </summary>
     public void Warp()
     {
-        Debug.Log("Attempting to Load Scene: "+level);
+        Debug.Log("Attempting to Load Scene: " + level);
         SceneManager.LoadScene(LevelList[level++]);
 
     }
 
-
+    /// <summary>
+    /// Icrement the progress counter
+    /// </summary>
     public void ProgressUp()
     {
         progress++;
-        Debug.Log("Progress Increased to: "+progress);
+        Debug.Log("Progress Increased to: " + progress);
     }
 
+    /// <summary>
+    /// Decrement the level counter when the player dies so that they are returned to the level they died on
+    /// </summary>
     public void Death()
     {
         level--;
-        Debug.Log("Level Decremented to: "+ level);
+        Debug.Log("Level Decremented to: " + level);
     }
 
+    public int GetProgress()
+    {
+        return progress;
+    }
 
 }
