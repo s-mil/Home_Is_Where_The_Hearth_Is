ad2d99e08c9885cd38509b1fbcf71fa4fe226582
Luke7490 <luke7490@gmail.com>
2019-01-27 19:51:29 +0000

    No Comments

---
+++ Assets/TextFadeIn.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class TextFadeIn : MonoBehaviour
+{
+    public Text text;
+    // Start is called before the first frame update
+    void Start()
+    {
+        StartCoroutine(waitToFade());
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    IEnumerator waitToFade()
+    {
+        yield return new WaitForSeconds(2f);
+        Color.Lerp (Color.clear, Color.white, 1 * Time.deltaTime);
+    }}
